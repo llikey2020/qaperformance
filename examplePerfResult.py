62453e2e9f6738b8db643807c2be88dc0c0cf5f5
Tony Chen <tonychen@sequoiadb.com>
2021-06-21 19:55:55 -0400

    add example result job

---
+++ examplePerfResult.py
@@ -0,0 +1,19 @@
+
+from pyspark.sql import SparkSession
+from pyspark.conf import SparkConf
+from pyspark.context import SparkContext
+
+if __name__ == "__main__":
+    """
+        Usage: pi [partitions]
+    """
+
+    spark = SparkSession\
+        .builder\
+        .appName("examplePerfResultApp")\
+        .getOrCreate()
+
+    f = open("exampleResult.txt", "r")
+    print(f.read())
+    spark.stop()
+
