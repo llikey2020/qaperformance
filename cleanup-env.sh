bdb7ace121b2ecc7954490316f8d5c4491f17b3b
Matt Van Dijk <matthew@sequoiadb.com>
2021-06-22 21:23:41 +0000

    Update cleanup-env.sh, setup-env.sh files

--- cleanup-env.sh
+++ cleanup-env.sh
@@ -3,5 +3,5 @@
 set -ex
 
 helm uninstall alluxio alluxio-charts/alluxio || true
-kubectl delete pod ${SPARK_DRIVER_POD_NAME} --wait || true
-kubectl delete pod ${HTTP_SVC} --wait || true
+kubectl delete pod ${SPARK_DRIVER_POD_NAME} --wait=true --ignore-not-found=true
+kubectl delete pod ${HTTP_SVC} --wait=true --ignore-not-found=true
