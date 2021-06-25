56e33bf355e85f262c48bf975949993b07b18b2d
Tony Chen <tonychen@sequoiadb.com>
2021-06-25 19:58:35 +0000

    Add -U option to rm and perform checkConsistency after

--- cleanup-env.sh
+++ cleanup-env.sh
@@ -2,7 +2,9 @@
 
 set -ex
 
-kubectl exec alluxio-master-0 -c alluxio-master -- alluxio fs rm -R /${SPARK_WAREHOUSE} || true
-kubectl exec alluxio-master-0 -c alluxio-master -- alluxio fs rm -R /${SPARK_DEPENDENCY_DIR} || true
+kubectl exec alluxio-master-0 -c alluxio-master -- alluxio fs rm -RU /${SPARK_WAREHOUSE} || true
+kubectl exec alluxio-master-0 -c alluxio-master -- alluxio fs checkConsistency -r /${SPARK_WAREHOUSE} || true
+kubectl exec alluxio-master-0 -c alluxio-master -- alluxio fs rm -RU /${SPARK_DEPENDENCY_DIR} || true
+kubectl exec alluxio-master-0 -c alluxio-master -- alluxio fs checkConsistency -r /${SPARK_DEPENDENCY_DIR} || true
 helm uninstall alluxio alluxio-charts/alluxio || true
 kubectl delete pod ${SPARK_DRIVER_POD_NAME} --wait=true --ignore-not-found=true
