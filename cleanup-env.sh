3e4908b57c5b5eb61383d3fe822c65326260947f
Tony Chen <tonychen@sequoiadb.com>
2021-06-25 20:13:30 +0000

    Remove checkConsistency

--- cleanup-env.sh
+++ cleanup-env.sh
@@ -3,8 +3,6 @@
 set -ex
 
 kubectl exec alluxio-master-0 -c alluxio-master -- alluxio fs rm -RU /${SPARK_WAREHOUSE} || true
-kubectl exec alluxio-master-0 -c alluxio-master -- alluxio fs checkConsistency -r /${SPARK_WAREHOUSE} || true
 kubectl exec alluxio-master-0 -c alluxio-master -- alluxio fs rm -RU /${SPARK_DEPENDENCY_DIR} || true
-kubectl exec alluxio-master-0 -c alluxio-master -- alluxio fs checkConsistency -r /${SPARK_DEPENDENCY_DIR} || true
 helm uninstall alluxio alluxio-charts/alluxio || true
 kubectl delete pod ${SPARK_DRIVER_POD_NAME} --wait=true --ignore-not-found=true
