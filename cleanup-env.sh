800db60c8c7e48f761c4555f9ccffc7314b896ee
Tony Chen <tonychen@sequoiadb.com>
2021-06-25 16:45:22 +0000

    Update cleanup-env.sh

--- cleanup-env.sh
+++ cleanup-env.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-helm uninstall alluxio alluxio-charts/alluxio || true
-kubectl delete pod ${SPARK_DRIVER_POD_NAME} --wait=true --ignore-not-found=true
 kubectl exec alluxio-master-0 -c alluxio-master -- alluxio fs rm -R /${SPARK_WAREHOUSE}
 kubectl exec alluxio-master-0 -c alluxio-master -- alluxio fs rm -R /${SPARK_DEPENDENCY_DIR}
+helm uninstall alluxio alluxio-charts/alluxio || true
+kubectl delete pod ${SPARK_DRIVER_POD_NAME} --wait=true --ignore-not-found=true
