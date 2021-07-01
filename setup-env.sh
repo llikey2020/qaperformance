36377cf46bf542e26fdfc4bb977cb479ace99673
Tony Chen <tonychen@sequoiadb.com>
2021-07-01 12:23:56 -0400

    spark history helm chart

--- setup-env.sh
+++ setup-env.sh
@@ -39,43 +39,38 @@ EOF
 helm repo add alluxio-charts https://alluxio-charts.storage.googleapis.com/openSource/2.6.0
 helm install alluxio -f alluxio.yaml alluxio-charts/alluxio --wait
 
-cat << EOF > history.yaml
-pvc:
-  # to use a file system path for Spark events dir, set 'enablePVC' to true and mention the
-  # name of an already created persistent volume claim in existingClaimName.
-  # The volume will be mounted on /data in the pod
-  enablePVC: false
-  existingClaimName: nfs-pvc
-  eventsDir: "/"
-
-# Settings for the sub-chart
-# When pvc.enablePVC is true, make sure:
-# pvc.existingClaimName == nfs.pvcName
-nfs:
-  enableExampleNFS: false
-  pvName: nfs-pv
-  pvcName: nfs-pvc
-s3:
-  enableS3: true
-  enableIAM: false
-  # Omit for IAM role-based or provider-based authentication.
-  secret:
-  # accessKeyName is an AWS access key ID. Omit for IAM role-based or provider-based authentication.
-  accessKeyName: ${AWS_ACCESS_KEY_ID}
-  # secretKey is AWS secret key. Omit for IAM role-based or provider-based authentication.
-  secretKeyName: ${AWS_SECRET_ACCESS_KEY}
-  logDirectory: ${ALLUXIO_UFS}spark-logs/performance
-  # custom s3 endpoint. Keep default for using aws s3 endpoint
-  endpoint: default
-EOF
-
 echo -n "${AWS_ACCESS_KEY_ID}" > aws-access-key
 echo -n "${AWS_SECRET_ACCESS_KEY}" > aws-secret-key
 kubectl create secret generic aws-secrets --from-file=aws-access-key --from-file=aws-secret-key
 
+cat << EOF > values.yaml
+s3:
+  enableS3: true
+  enableIAM: false
+  secret: aws-secrets
+  logDirectory: ${ALLUXIO_UFS}spark-logs/performance
+  # accessKeyName is an AWS access key ID. Omit for IAM role-based or provider-based authentication.
+  accessKeyName: aws-access-key
+  # secretKey is AWS secret key. Omit for IAM role-based or provider-based authentication.
+  secretKeyName: aws-secret-key
+
+gcs:
+  enableGCS: false
+  logDirectory: gs://spark-hs/
+
+pvc:
+  enablePVC: false
+
+nfs:
+  enableExampleNFS: false
+
+service:
+  type: NodePort
+  port: 18080
+  annotations: {}
+EOF
+
 helm repo add stable https://charts.helm.sh/stable
-helm install spark-history stable/spark-history-server -f history.yaml
+helm install -f values.yaml stable/spark-history-server --namespace performance-47-testing
 
 kubectl exec alluxio-master-0 -c alluxio-master -- alluxio fs mkdir /${SPARK_EVENTLOG_DIR} || true
-#ls /opt/spark
-#/opt/spark/sbin/start-history-server.sh
