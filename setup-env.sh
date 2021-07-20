5a0452bd8b4efd5429d3f516fb6e358ce4d67342
Tony Chen <tonychen@sequoiadb.com>
2021-07-20 17:50:54 -0400

    Use helm chart to deploy history server

--- setup-env.sh
+++ setup-env.sh
@@ -95,5 +95,7 @@ spec:
 EOF
 
 if [[ ${SPARK_HISTORY_SERVER_ENABLED} == "true" ]]; then
-  kubectl apply -f history-server.yaml
+  wget --header="JOB-TOKEN: ${CI_JOB_TOKEN}" ${CI_API_V4_URL}/projects/55/packages/generic/history-server-helm-chart/0.1.0/history-server-0.1.0.tgz
+  tar -zxf history-server-0.1.0.tgz
+  helm install history-server --set eventLog.alluxioService=${ALLUXIO_SVC} --set eventLog.dir=${SPARK_EVENTLOG_DIR} history-server/ --wait
 fi
